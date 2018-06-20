06ba4047e7e6e1d9b7acaf3100891844b8f1522b
Daniil Kistanov <daniilkistanov@csus.edu>
2018-06-19 22:16:52 -0700

    wrote comments and deleted didReceiveMemoryWarning()

--- Accentos/ViewController.swift
+++ Accentos/ViewController.swift
@@ -3,12 +3,16 @@ import Foundation
 
 class ViewController: UIViewController {
     
+    //the three fields that are responsible
+    //find the word
+    //find the json file matching the first letter
     @IBOutlet weak var result: UILabel!
     @IBOutlet weak var wordTyped: UITextField!
+    
     @IBAction func wordSubmitted(_ sender: Any) {
         let word = wordTyped.text!
         let dict = findDict(firstChar: word[(word.startIndex)])
-        if dict[word] != nil {
+        if dict[word] != nil && !dict.isEmpty {
             if word == dict[word] {
                 result.text = "No accent here"
             } else {
@@ -23,11 +27,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+    //function that build a dictionary out of a json file
+    //I prepared 26 json with 160000 spanish words
+    //based on that letter it returns 
     func findDict(firstChar:Character) -> [String:String] {
         do {
             let path = Bundle.main.path(forResource: String(firstChar), ofType: "json")
