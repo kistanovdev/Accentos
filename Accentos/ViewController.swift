1c24933076a0733c4cab41e9e51c0cd3139a783a
Daniil Kistanov <daniilkistanov@csus.edu>
2018-06-19 21:18:25 -0700

    First demo of the app All the json dictionaries and the code to find and determine if the word has an accent

--- Accentos/ViewController.swift
+++ Accentos/ViewController.swift
@@ -1,25 +1,44 @@
-//
-//  ViewController.swift
-//  Accentos
-//
-//  Created by Daniil on 6/18/18.
-//  Copyright © 2018 Daniil Kistanov. All rights reserved.
-//
-
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var wordTyped: UITextField!
+    @IBAction func wordSubmitted(_ sender: Any) {
+        let word = wordTyped.text!
+        let dict = findDict(firstChar: word[(word.startIndex)])
+        if dict[word] != nil {
+            if word == dict[word] {
+                result.text = "No accent here"
+            } else {
+                result.text = dict[word]
+            }
+        } else {
+            result.text = "No such word"
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func findDict(firstChar:Character) -> [String:String] {
+        do {
+            let path = Bundle.main.path(forResource: String(firstChar), ofType: "json")
+            let url  = URL(fileURLWithPath: path!)
+            let data = try Data(contentsOf: url)
+            let words = try JSONDecoder().decode([String:String].self, from: data)
+            return words
+        }
+        catch {
+            return [:]
+        }
+    }
 }
 
