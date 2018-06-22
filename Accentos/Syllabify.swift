11fc3214f0be277134c6a6dbc8feb5ade56eaba2
Daniil Kistanov <daniilkistanov@csus.edu>
2018-06-22 15:18:37 -0700

    added the input for the Syllabifyer view

--- Accentos/Syllabify.swift
+++ Accentos/Syllabify.swift
@@ -5,6 +5,11 @@
 //  Created by Daniil on 6/22/18.
 //  Copyright © 2018 Daniil Kistanov. All rights reserved.
 //
+//View that takes a word and then splits it into syllables
+//Uses special algorith developed by
+//* Hernández-Figueroa, Z; Rodríguez-Rodríguez, G; Carreras-Riudavets, F (2009). *
+//* Separador de sílabas del español - Silabeador TIP.                           *
+//* Available at http://tip.dis.ulpgc.es
 
 import Foundation
 import UIKit
@@ -13,5 +18,9 @@ class Syllabify: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    @IBOutlet weak var wordTyped: UITextField!
+    
+    @IBAction func wordWasSumbitted(_ sender: Any) {
     }
 }
