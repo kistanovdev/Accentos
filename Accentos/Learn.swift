e4268b19034ba0c14c7b70d514fd525b846c43c9
Daniil Kistanov <daniilkistanov@csus.edu>
2018-07-02 17:19:35 -0700

    added a return button for Learn View

---
+++ Accentos/Learn.swift
@@ -0,0 +1,17 @@
+//
+//  Learn.swift
+//  Accentos
+//
+//  Created by Daniil on 7/2/18.
+//  Copyright © 2018 Daniil Kistanov. All rights reserved.
+//
+
+import UIKit
+
+class Learn: UIViewController {
+    
+    @IBAction func closeView(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
